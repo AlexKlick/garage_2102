becdb01bb5596df2922f550fd96de0191d8392f4
Alex Klick <aklick12@gmail.com>
2021-02-25 09:24:09 -0700

    owner instnace var test setup

---
+++ test/owner_test.rb
@@ -0,0 +1,12 @@
+require './lib/car'
+require './lib/owner'
+require 'minitest/autorun'
+require 'minitest/pride'
+class CarTest < Minitest::Test
+  def test_instance_vars
+    owner_1 = Owner.new('Regina George', 'Heiress')
+    assert_equal owner_1.name, "Regina Jones"
+    assert_equal owner_1.occupation, "Heiress"
+    assert_equal owner_1.cars, []
+  end
+end
